17a006567fd6a091c2624dd22e05188dfefef20f
Oscar Rodriguez <oscar88@gmail.com>
2017-01-23 18:33:13 -0600

    index y functions, limpio sin las funciones de las versión pasada

--- Functions.php
+++ Functions.php
@@ -12,7 +12,6 @@ class Functions
 		$this->message = $message;
 		$this->urlWebhook = $urlWebhook;
 		$this->connectiondb = $connectiondb = new ConnectionDb();
-		
 	}
 	
 	public function sendTextMessage ($reply)
